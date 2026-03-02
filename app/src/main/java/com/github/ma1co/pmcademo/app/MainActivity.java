d7661735a8f82da6171babd48d2eaf38c947add4
James <jbuch84@gmail.com>
2026-03-01 22:53:09 -0500

    bigchange

--- app/src/main/java/com/github/ma1co/pmcademo/app/MainActivity.java
+++ app/src/main/java/com/github/ma1co/pmcademo/app/MainActivity.java
@@ -2,27 +2,46 @@ package com.github.ma1co.pmcademo.app;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.KeyEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.TextView;
 import com.sony.scalar.hardware.CameraEx;
 import com.sony.scalar.sysutil.ScalarInput;
 import java.io.IOException;
+import java.util.List;
 
-public class MainActivity extends Activity implements SurfaceHolder.Callback, CameraEx.ShutterListener {
+public class MainActivity extends Activity implements SurfaceHolder.Callback, CameraEx.ShutterListener, CameraEx.ShutterSpeedChangeListener {
     private CameraEx mCameraEx;
     private SurfaceHolder mSurfaceHolder;
+    private TextView tvShutter, tvAperture, tvISO, tvExposure;
+    private int curIso;
+    private List<Integer> supportedIsos;
+    
+    enum DialMode { shutter, aperture, iso, exposure }
+    private DialMode mDialMode = DialMode.shutter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SurfaceView surfaceView = new SurfaceView(this);
+        setContentView(R.layout.activity_main);
+        
+        SurfaceView surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
         mSurfaceHolder = surfaceView.getHolder();
         mSurfaceHolder.addCallback(this);
         mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-        setContentView(surfaceView);
+        
+        tvShutter = (TextView) findViewById(R.id.tvShutter);
+        tvAperture = (TextView) findViewById(R.id.tvAperture);
+        tvISO = (TextView) findViewById(R.id.tvISO);
+        tvExposure = (TextView) findViewById(R.id.tvExposure);
+        
+        setDialMode(DialMode.shutter);
     }
 
     @Override
@@ -30,70 +49,75 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback, Ca
         super.onResume();
         mCameraEx = CameraEx.open(0, null);
         mCameraEx.setShutterListener(this);
+        mCameraEx.setShutterSpeedChangeListener(this);
         mCameraEx.startDirectShutter();
         
-        // PERSISTENCE FIX: Tells the camera to return here after Playback mode
-        notifyStatus(true);
+        // Settings Sync
+        CameraEx.ParametersModifier pm = mCameraEx.createParametersModifier(mCameraEx.getNormalCamera().getParameters());
+        supportedIsos = (List<Integer>) pm.getSupportedISOSensitivities();
+        curIso = pm.getISOSensitivity();
+        
+        sendSonyBroadcast(true);
+        updateUI();
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        notifyStatus(false);
-        if (mCameraEx != null) {
-            mCameraEx.getNormalCamera().stopPreview();
-            mCameraEx.release();
-            mCameraEx = null;
-        }
-    }
-
-    private void notifyStatus(boolean resume) {
-        // This is the "Stay Alive" broadcast found in BetterManual/Demo apps
+    private void sendSonyBroadcast(boolean active) {
         Intent intent = new Intent("com.android.server.DAConnectionManagerService.AppInfoReceive");
         intent.putExtra("package_name", getPackageName());
-        intent.putExtra("class_name", getClass().getName());
-        // If resume is true, we tell the camera to keep us in the loop
-        intent.putExtra("resume_key", resume ? new String[]{"on"} : new String[]{});
+        intent.putExtra("resume_key", active ? new String[]{"on"} : new String[]{});
         sendBroadcast(intent);
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        // TRASH BUTTON: Standard Exit
-        if (keyCode == ScalarInput.ISV_KEY_DELETE) {
-            finish();
+        int scanCode = event.getScanCode();
+        if (scanCode == ScalarInput.ISV_KEY_DELETE) { finish(); return true; }
+        
+        // Cycle through Shutter -> Aperture -> ISO using the DOWN key
+        if (scanCode == ScalarInput.ISV_KEY_DOWN) {
+            if (mDialMode == DialMode.shutter) setDialMode(DialMode.aperture);
+            else if (mDialMode == DialMode.aperture) setDialMode(DialMode.iso);
+            else setDialMode(DialMode.shutter);
             return true;
         }
-
-        // MENU or CENTER BUTTON: Open Native Sony Settings
-        // This allows you to change Shutter/Aperture/ISO
-        if (keyCode == ScalarInput.ISV_KEY_MENU || keyCode == ScalarInput.ISV_KEY_ENTER) {
-            try {
-                startActivity(new Intent("com.sony.scalar.app.setting.SETTING"));
-            } catch (Exception e) {
-                // If native settings call fails
-            }
-            return true;
-        }
-
         return super.onKeyDown(keyCode, event);
     }
 
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        try {
-            if (mCameraEx != null) {
-                Camera cam = mCameraEx.getNormalCamera();
-                cam.setPreviewDisplay(holder);
-                cam.startPreview();
-            }
-        } catch (IOException e) {}
+    // BetterManual logic for the camera control dial
+    protected boolean onUpperDialChanged(int value) {
+        if (mDialMode == DialMode.shutter) {
+            if (value > 0) mCameraEx.incrementShutterSpeed(); else mCameraEx.decrementShutterSpeed();
+        } else if (mDialMode == DialMode.aperture) {
+            if (value > 0) mCameraEx.incrementAperture(); else mCameraEx.decrementAperture();
+        }
+        return true;
+    }
+
+    private void setDialMode(DialMode mode) {
+        mDialMode = mode;
+        tvShutter.setTextColor(mode == DialMode.shutter ? Color.GREEN : Color.WHITE);
+        tvAperture.setTextColor(mode == DialMode.aperture ? Color.GREEN : Color.WHITE);
+        tvISO.setTextColor(mode == DialMode.iso ? Color.GREEN : Color.WHITE);
+    }
+
+    private void updateUI() {
+        CameraEx.ParametersModifier pm = mCameraEx.createParametersModifier(mCameraEx.getNormalCamera().getParameters());
+        Pair<Integer, Integer> speed = pm.getShutterSpeed();
+        tvShutter.setText(speed.first + "/" + speed.second);
+        tvISO.setText("ISO " + curIso);
     }
 
     @Override
-    public void onShutter(int i, CameraEx cameraEx) {}
+    public void onShutterSpeedChange(CameraEx.ShutterSpeedInfo info, CameraEx camera) {
+        tvShutter.setText(info.currentShutterSpeed_n + "/" + info.currentShutterSpeed_d);
+    }
+
     @Override
-    public void surfaceChanged(SurfaceHolder h, int f, int w, int h1) {}
-    @Override
-    public void surfaceDestroyed(SurfaceHolder h) {}
+    public void surfaceCreated(SurfaceHolder h) {
+        try { mCameraEx.getNormalCamera().setPreviewDisplay(h); mCameraEx.getNormalCamera().startPreview(); } catch (Exception e) {}
+    }
+    
+    @Override public void onShutter(int i, CameraEx c) {}
+    @Override public void surfaceChanged(SurfaceHolder h, int f, int w, int h1) {}
+    @Override public void surfaceDestroyed(SurfaceHolder h) {}
 }
