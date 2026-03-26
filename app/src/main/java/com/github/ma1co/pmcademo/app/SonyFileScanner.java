2a7da3681583294fad259e26b0325f65ad5f0328
James <jbuch84@gmail.com>
2026-03-26 16:42:46 -0400

    filescanner fix

--- app/src/main/java/com/github/ma1co/pmcademo/app/SonyFileScanner.java
+++ app/src/main/java/com/github/ma1co/pmcademo/app/SonyFileScanner.java
@@ -1,4 +1,77 @@
-private void findNewestFile(boolean triggerCallback) {
+package com.github.ma1co.pmcademo.app;
+
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.util.Log;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SonyFileScanner {
+    private ScannerCallback mCallback;
+    private String lastSeenFilePath = "";
+    
+    private HandlerThread scannerThread;
+    private Handler backgroundHandler;
+    private Handler mainHandler;
+    private boolean isPolling = false;
+
+    public interface ScannerCallback {
+        void onNewPhotoDetected(String filePath);
+        boolean isReadyToProcess(); 
+    }
+
+    public SonyFileScanner(ScannerCallback callback) {
+        this.mCallback = callback;
+        this.mainHandler = new Handler(Looper.getMainLooper()); 
+        
+        Log.d("JPEG.CAM", "SonyFileScanner initialized. Root: " + Filepaths.getDcimDir().getAbsolutePath());
+        
+        findNewestFile(false); 
+        
+        scannerThread = new HandlerThread("FileScannerThread");
+        scannerThread.start();
+        backgroundHandler = new Handler(scannerThread.getLooper());
+        
+        start();
+    }
+
+    public void start() {
+        if (!isPolling) {
+            isPolling = true;
+            scheduleNextPoll();
+        }
+    }
+
+    public void stop() {
+        isPolling = false;
+        if (backgroundHandler != null) {
+            backgroundHandler.removeCallbacksAndMessages(null);
+        }
+    }
+
+    public void checkNow() {
+        if (backgroundHandler != null) {
+            backgroundHandler.post(new Runnable() {
+                @Override public void run() { findNewestFile(true); }
+            });
+        }
+    }
+
+    private void scheduleNextPoll() {
+        backgroundHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (isPolling) {
+                    findNewestFile(true);
+                    scheduleNextPoll(); 
+                }
+            }
+        }, 1000);
+    }
+
+    private void findNewestFile(boolean triggerCallback) {
         File dcimDir = Filepaths.getDcimDir(); 
         if (!dcimDir.exists() || !dcimDir.isDirectory()) {
             if (triggerCallback) Log.e("JPEG.CAM", "DCIM directory not found: " + dcimDir.getAbsolutePath());
@@ -55,3 +128,4 @@ private void findNewestFile(boolean triggerCallback) {
             }
         }
     }
+}
